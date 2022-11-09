84c5b5cff8e60b73f5ee6a609e76aebdfe320fd5
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-11-10 01:57:58 +0900

    Day2Commit2

--- src/main/java/tk/newsoulmate/domain/dao/BoardDao.java
+++ src/main/java/tk/newsoulmate/domain/dao/BoardDao.java
@@ -2,6 +2,7 @@ package tk.newsoulmate.domain.dao;
 
 import tk.newsoulmate.domain.vo.Board;
 import tk.newsoulmate.domain.vo.PageInfo;
+import tk.newsoulmate.domain.vo.Reply;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -48,45 +49,6 @@ public class BoardDao {
         }
 
         return result;
-    }
-    public ArrayList<Board> selectQnAList(Connection conn, PageInfo pi) {
-
-        ArrayList<Board> list = new ArrayList<>();
-
-        PreparedStatement psmt = null;
-
-        ResultSet rset = null;
-
-        String sql = prop.getProperty("selectQnAList");
-
-        try {
-            psmt = conn.prepareStatement(sql);
-
-            int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
-            int endRow = startRow + pi.getBoardLimit() - 1;
-
-            psmt.setInt(1, startRow);
-            psmt.setInt(2, endRow);
-
-            rset = psmt.executeQuery();
-
-            while (rset.next()) {
-                list.add(Board.selectQnAList(rset.getString("RESULTSTATUS")
-                        , rset.getInt("BOARD_NO")
-                        , rset.getString("BOARD_TITLE")
-                        , rset.getDate("CREATE_DATE")
-                        , rset.getString("MEMBER_NAME")));
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close(rset);
-            close(psmt);
-        }
-        return list;
-
     }
 
     public ArrayList<Board> selectQnAList(Connection conn, PageInfo pi) {
@@ -230,7 +192,34 @@ public class BoardDao {
         }
         return list;
     }
+    public int selectListCount(Connection conn, String categoryName){
 
+        int listCount = 0;
+
+        PreparedStatement psmt = null;
+
+        ResultSet rset = null;
+
+        String sql = prop.getProperty("selectListCount");
+
+        try {
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, categoryName);
+
+            rset = psmt.executeQuery();
+
+            if(rset.next()){
+                listCount = rset.getInt("cnt");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rset);
+            close(psmt);
+        }
+        return listCount;
+    }
 
 
 }
